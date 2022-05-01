86380fc3fe069a5ec9303869e69b018d9fe0a935
rajendrakrshaw <rajendra250601@gmail.com>
2022-05-01 19:38:27 +0530

    Added global styling and created sidebar component

---
+++ client/pokechain/components/sidebar.js
@@ -0,0 +1,10 @@
+import styles from '../styles/sidebar.module.css'
+
+import {useEffect, useState} from 'react'
+
+import { useRouter } from 'next/router'
+
+const Sidebar = () => {
+    return <div className='{styles.wrapper}'>Sidebar</div>
+}
+export default Sidebar
